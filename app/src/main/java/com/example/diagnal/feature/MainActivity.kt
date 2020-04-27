83eddddaf0f9a37a9174df7e03a8009aa5a98047
Balamuvesh B <balamuvesh@qburst.com>
2020-04-27 14:07:53 +0530

    Moved MainActivity to feature/main

--- app/src/main/java/com/example/diagnal/feature/MainActivity.kt
+++ app/src/main/java/com/example/diagnal/feature/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.diagnal
+package com.example.diagnal.feature
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.diagnal.R
 
 class MainActivity : AppCompatActivity() {
 
