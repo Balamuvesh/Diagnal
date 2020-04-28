6f9668d7ff87e3afc0802d86215c03e9826669fe
Balamuvesh B <balamuvesh@qburst.com>
2020-04-28 12:17:37 +0530

    Added onClick functionality to the search functionality

---
+++ app/src/main/java/com/example/diagnal/common/extensions/TextInputEditTextExtensions.kt
@@ -0,0 +1,19 @@
+package com.example.diagnal.common.extensions
+
+import android.app.Activity
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import kotlinx.android.synthetic.main.fragment_main.*
+
+fun EditText.showKeyboard(){
+    val imm =
+        this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+    imm?.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+}
+
+fun EditText.hideKeyboard(){
+    val imm =
+        this.context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(this.windowToken, 0)
+}
--- app/src/main/java/com/example/diagnal/feature/main/MainFragment.kt
+++ app/src/main/java/com/example/diagnal/feature/main/MainFragment.kt
@@ -1,19 +1,27 @@
 package com.example.diagnal.feature.main
 
+import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
+import android.view.View.OnTouchListener
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.diagnal.R
+import com.example.diagnal.common.extensions.hideKeyboard
+import com.example.diagnal.common.extensions.showKeyboard
 import com.example.diagnal.common.ui.ViewState
 import com.example.diagnal.data.movie.Movie
 import com.example.diagnal.data.movie.MovieListResponseModel
 import kotlinx.android.synthetic.main.fragment_main.*
+
 
 class MainFragment : Fragment() {
     private lateinit var mContext: Context
@@ -38,6 +46,7 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
@@ -58,9 +67,45 @@ class MainFragment : Fragment() {
                 populateView(movieListResponseModel.page.contentItems.movieList)
             })
 
+        setupViews()
+
         movieListViewModel.getMoviesList()
     }
 
+    private fun setupViews() {
+        img_search.setOnClickListener {
+            img_search.visibility = View.GONE
+            appbar_title.visibility = View.GONE
+            search_view.visibility = View.VISIBLE
+            search_view.requestFocus()
+            if(search_view.requestFocus()) {
+                search_view.showKeyboard()
+            }
+        }
+
+        /**
+         * [search_view] is an EditText with a right drawable. The below code sets a touch listener
+         *  to the right drawable.
+         */
+        search_view.setOnTouchListener(OnTouchListener { v, event ->
+//            val DRAWABLE_LEFT = 0
+//            val DRAWABLE_TOP = 1
+            val DRAWABLE_RIGHT = 2
+//            val DRAWABLE_BOTTOM = 3
+            if (event.action == MotionEvent.ACTION_UP) {
+                if (event.rawX >= search_view.right - search_view.compoundDrawables[DRAWABLE_RIGHT].bounds.width()
+                ) {
+                    search_view.text = null
+                    img_search.visibility = View.VISIBLE
+                    appbar_title.visibility = View.VISIBLE
+                    search_view.visibility = View.GONE
+                    search_view.hideKeyboard()
+                    return@OnTouchListener true
+                }
+            }
+            false
+        })
+    }
 
     private fun populateView(movieList: List<Movie>) {
         val gridLayoutManager = GridLayoutManager(mContext, 3, GridLayoutManager.VERTICAL, false)
